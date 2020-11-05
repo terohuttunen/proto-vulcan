c43e372a7587f4daeead1a6fb056daf5cc538363
Tero Huttunen <tero.huttunen@ultic.io>
2020-11-05 16:33:50 +0200

    Fix a bug in permuteo, rewrite it with pattern matching, and add a unit test.

--- src/relation/permuteo.rs
+++ src/relation/permuteo.rs
@@ -1,9 +1,6 @@
 use crate::goal::Goal;
 use crate::lterm::LTerm;
-use crate::operator::conde;
-use crate::relation::conso;
 use crate::relation::rembero;
-use crate::relation::resto;
 use crate::user::UserState;
 use std::rc::Rc;
 
@@ -13,15 +10,31 @@ pub fn permuteo<U: UserState>(xl: &Rc<LTerm>, yl: &Rc<LTerm>) -> Rc<dyn Goal<U>>
     let yl = Rc::clone(yl);
     proto_vulcan!(
         closure {
-            conde {
-                [xl == [], yl == []],
-                |x, xs, ys| {
-                    conso(x, xs, xl),
-                    resto(yl, ys),
+            match [xl, yl] {
+                [[], []] => ,
+                [[x | xs], _] => |ys| {
                     permuteo(xs, ys),
                     rembero(x, yl, ys),
                 }
             }
         }
     )
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+    use super::*;
+
+    #[test]
+    fn test_permuteo_1() {
+        let query = proto_vulcan_query!(|q| { permuteo([1, 2], q) });
+        let mut iter = query.run();
+        assert_eq!(iter.next().unwrap().q, lterm!([]));
+        assert_eq!(iter.next().unwrap().q, lterm!([1]));
+        assert_eq!(iter.next().unwrap().q, lterm!([1, 2]));
+        assert_eq!(iter.next().unwrap().q, lterm!([2]));
+        assert_eq!(iter.next().unwrap().q, lterm!([2, 1]));
+        assert!(iter.next().is_none());
+    }
 }
